765a9bc3d059f83c789d39c8f36c08975e7701f3
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-01-11 17:31:23 +0200

    add /

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/currency-exchange">
+    <BrowserRouter basename="/currency-exchange/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
