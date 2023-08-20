0d49105edaa797c4c5688e5a4ee36739be819431
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-08-20 13:45:28 +0300

    fixed request

--- src/services/currencyExchange.ts
+++ src/services/currencyExchange.ts
@@ -1,37 +1,41 @@
 import axios from 'axios';
 
-const KEY: any = process.env.REACT_APP_API_KEY_PLAYER;
+const KEY: any = process.env.REACT_APP_API_KEY_EXCHANGE;
 
-const myHeaders = new Headers();
-myHeaders.append('apikey', KEY);
+const currencyExchangeApi = axios.create({
+  baseURL: 'https://api.apilayer.com/exchangerates_data',
+});
 
-// const currencyExchangeApi = axios.create({
-//   baseURL: 'https://api.apilayer.com/exchangerates_data',
-// });
-
-const baseURL = 'https://api.apilayer.com/exchangerates_data';
-
-const requestOptions: RequestInit = {
-  method: 'GET',
+const requestOptions = {
+  headers: { apikey: KEY },
   redirect: 'follow',
-  headers: myHeaders,
 };
 
-export async function exchangeCurrency(to: any, from: any, amount: any) {
+export async function exchangeCurrency(
+  to: string,
+  from: string,
+  amount: string
+) {
   try {
-    const data = await fetch(
-      `${baseURL}/convert?to=${to}&from=${from}&amount=${amount}`,
+    const { data } = await currencyExchangeApi.get(
+      `/convert?to=${to}&from=${from}&amount=${amount}`,
       requestOptions
     );
 
-    return data.json();
+    return data;
   } catch (error) {
     console.log(error);
   }
 }
 export async function getLatest(base: string) {
-  return fetch(
-    `${baseURL}/latest?symbols=USD,GBP,EUR&base=${base}`,
-    requestOptions
-  ).then(response => response.json());
+  try {
+    const { data } = await currencyExchangeApi.get(
+      `$/latest?symbols=USD,GBP,EUR&base=${base}`,
+      requestOptions
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
 }
